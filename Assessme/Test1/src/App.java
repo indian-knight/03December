74d1dee6eeae650e8f2f16070cfee564706d851f
indian-knight <152703283+indian-knight@users.noreply.github.com>
2023-12-03 21:13:24 +0530

    Added Changes for Test1
    Date : 03 december

---
+++ Assessme/Test1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("SHOPPING LIST");
+        System.out.println("\t- Milk");
+        System.out.println("\t\t> Semi-skimmed");
+        System.out.println("\t\t> Medium");
+        System.out.println("\t- Eggs");
+        System.out.println("\t\t> Free-range");
+        System.out.println("\t- Bread");
+    }
+}
