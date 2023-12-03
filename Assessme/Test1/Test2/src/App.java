b0ae7c0e4758c78aaaccf6cc0552d71d01b5acfd
indian-knight <152703283+indian-knight@users.noreply.github.com>
2023-12-03 21:35:03 +0530

    printing all things in one line

---
+++ Assessme/Test1/Test2/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("SHOPPING LIST\n\t- Milk\n\t\t> Semi-skimmed\n\t\t> Medium\n\t- Eggs\n\t\t> Free-range\n\t- Bread"); 
+    }
+}
